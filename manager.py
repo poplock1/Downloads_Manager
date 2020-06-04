d423a261135d8a218b0b14533e9d14ee021e987c
Krystian Plich <krypli94@gmail.com>
2020-06-04 20:07:52 +0200

    Add user path finding

--- manager.py
+++ manager.py
@@ -1,36 +1,50 @@
 import os
+import settings
 import shutil
 
-downloads_directory = '/home/poplock/Pobrane/'
-documents_directory = '/home/poplock/Dokumenty'
-movies_directory = '/home/poplock/Wideo'
-pictures_direcotry = '/home/poplock/Obrazy'
-music_directory = '/home/poplock/Muzyka'
-archive_directory = '/home/poplock/Archiwa'
 
-document_file_extensions_list = ['.doc', '.docx', '.txt', '.pdf', '.odt']
-movie_file_extensions_list = ['.mp4', '.mov', '.avi', '.flv', '.mkv']
-music_file_extensions_list = ['.mp3', '.wav', '.flac']
-pictures_file_extensions_list = ['.jpg', '.gif', '.png']
-archive_file_extensions_list = ['.7z', '.zip', '.rar']
+language = os.getenv('LANG')
+
+if language[:2] == 'pl':
+
+    downloads_directory = os.path.expanduser('~/Pobrane')
+    documents_directory = os.path.expanduser('~/Dokumenty')
+    movies_directory = os.path.expanduser('~/Wideo')
+    pictures_direcotry = os.path.expanduser('~/Obrazy')
+    music_directory = os.path.expanduser('~/Muzyka')
+    archive_directory = os.path.expanduser('~/Archiwa')
+
+if language[:2] == 'en':
+
+    downloads_directory = os.path.expanduser('~/Downloads')
+    documents_directory = os.path.expanduser('~/Documents')
+    movies_directory = os.path.expanduser('~/Video')
+    pictures_direcotry = os.path.expanduser('~/Pictures')
+    music_directory = os.path.expanduser('~/Music')
+    archive_directory = os.path.expanduser('~/Archives')
 
 for filename in os.listdir(downloads_directory):
-    for extension in document_file_extensions_list:
+    for extension in settings.document_file_extensions_list:
         if filename.endswith(extension):
-            shutil.move(f"{downloads_directory}/{filename}", f"{documents_directory}/{filename}")
+            shutil.move(f"{downloads_directory}/{filename}",
+                        f"{documents_directory}/{filename}")
 for filename in os.listdir(downloads_directory):
-    for extension in movie_file_extensions_list:
+    for extension in settings.movie_file_extensions_list:
         if filename.endswith(extension):
-            shutil.move(f"{downloads_directory}/{filename}", f"{movies_directory}/{filename}")
+            shutil.move(f"{downloads_directory}/{filename}",
+                        f"{movies_directory}/{filename}")
 for filename in os.listdir(downloads_directory):
-    for extension in music_file_extensions_list:
+    for extension in settings.music_file_extensions_list:
         if filename.endswith(extension):
-            shutil.move(f"{downloads_directory}/{filename}", f"{music_directory}/{filename}")
+            shutil.move(f"{downloads_directory}/{filename}",
+                        f"{music_directory}/{filename}")
 for filename in os.listdir(downloads_directory):
-    for extension in pictures_file_extensions_list:
+    for extension in settings.pictures_file_extensions_list:
         if filename.endswith(extension):
-            shutil.move(f"{downloads_directory}/{filename}", f"{pictures_direcotry}/{filename}")
+            shutil.move(f"{downloads_directory}/{filename}",
+                        f"{pictures_direcotry}/{filename}")
 for filename in os.listdir(downloads_directory):
-    for extension in archive_file_extensions_list:
+    for extension in settings.archive_file_extensions_list:
         if filename.endswith(extension):
-            shutil.move(f"{downloads_directory}/{filename}", f"{archive_directory}/{filename}")
+            shutil.move(f"{downloads_directory}/{filename}",
+                        f"{archive_directory}/{filename}")
