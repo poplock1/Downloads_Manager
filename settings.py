bbadf1dbe399167101e9570b533e2877b7dd5136
Krystian Plich <krypli94@gmail.com>
2020-06-04 20:02:26 +0200

    Add settings

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Lists of file extensions
+
+document_file_extensions_list = [
+    '.doc', '.docx', '.txt', '.pdf', '.odt', 'xlsx']
+movie_file_extensions_list = ['.mp4', '.mov', '.avi', '.flv', '.mkv']
+music_file_extensions_list = ['.mp3', '.wav', '.flac']
+pictures_file_extensions_list = ['.jpg', '.gif', '.png']
+archive_file_extensions_list = ['.7z', '.zip', '.rar']
